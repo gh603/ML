4665beb5f1846ee3df9e69f21be2c3d1195ec4bc
gh603 <gehui603@gmail.com>
2017-09-22 00:17:05 -0500

    Adding feature to grow decision tree

--- ID3.py
+++ ID3.py
@@ -20,6 +20,8 @@ def getAttributes(data):
 # Let x denotes explanatory variable, and y denotes resposne variable.
 # We need to get the counts of (X, Y): (True, True), (True, False), (False, True), (False, False); 
 def getCounts(data): 
+    if(data == None or len(data) == 0): 
+        return [[0,0,0,0]]
     nrow = len(data)
     ncol = len(data[0])
     counts = [[0 for i in range(4)] for i in range(ncol)]
@@ -38,6 +40,7 @@ def getCounts(data):
 # Given data, calculate the entropy of the response variable.
 def calEntropy(numT, numF): 
     total = numT + numF
+    if(total == 0): return 0
     pT = percentage(numT, total)
     pF = percentage(numF, total)
     return -1 * pT * log2(pT) - pF * log2(pF)
@@ -67,6 +70,7 @@ def calH(counts, i):
     count = counts[i]
     numT = count[0] + count[1]
     numF = count[2] + count[3]
+    if(numT == 0 or numF == 0): return 0
     return calEntropy(count[0], count[1]) * percentage(numT, numT + numF) + calEntropy(count[2], count[3]) * percentage(numF, numT + numF)
 
 # Divide the original dataset into two subset according to the value of an attribute i
@@ -85,32 +89,61 @@ def divMatrix(data, maxId):
 ################ Methods to build up trees ###################################
 # To implement a tree, we need a class of TreeNode
 class TreeNode(object): 
-    def __init__(self, attrId, attrName, nodeData, leftChild, rightChild, level):
-        self.attrId = attrId
+    def __init__(self, attrName, attrVal, nodeData, leftChild, rightChild, level, outputClass):
         self.attrName = attrName
+        self.attrVal = attrVal
         self.nodeData = nodeData
         self.leftChild = leftChild
         self.rightChild = rightChild
         self.level = level
+        self.outputClass = outputClass
+
+    def toString(self): 
+        if(self.level == 0): return ""
+        s = ''
+        if(self.level > 0): 
+            for i in range(self.level - 1): 
+                s = s + '|'
+        s = s + self.attrName + " = " + self.attrVal + ":"
+        if(self.leftChild == None and self.rightChild == None): 
+            s = s + self.outputClass
+        return s
+
 
 # Method to build up a Decision tree 
-def buildTree(data, header, level): 
+def buildTree(data, header, level, attrName, attrVal): 	
+    print 'calling buildeTree(data, header, ', level, ',', attrVal,')'
     #Find the attribute with most information gain
     counts = getCounts(data)
+    #Stop building tree if reaching the leaf: only single class included or no more attributes to divide
+    yDist = counts[len(counts) - 1]
+    dataEntropy = calEntropy(yDist[0] + yDist[1], yDist[2] + yDist[3])
+
+    if(dataEntropy == 0 or dataEntropy == -0 or len(counts) == 1): 
+        outputClass = "0"
+        if(yDist[2] + yDist[3] == 0): 
+            outputClass = "1"
+        return TreeNode(attrName, attrVal, data, None, None, level, outputClass); 
+
     maxId = maximizedIG(counts)
     #Divide the data into two sub data according to value of the attribute with most information gain
     dataOne, dataTwo = divMatrix(data, maxId)
+
     #Two sub data are used as the left child and right child of node
-    left = buildTree(dataOne, header, level + 1)
-    right = buildTree(dataTwo, header, level + 1)
+    left = buildTree(dataOne, header, level + 1, header[maxId], "0")
+    right = buildTree(dataTwo, header, level + 1, header[maxId], "1")
 
     #Return the node
-    node = TreeNode(maxid, header[maxId], data, left, right, level)
+    node = TreeNode(attrName, attrVal, data, left, right, level, None)
     return node
 
 # Method to print a Decision Tree
-def printTree(): 
-	print hell0; 
+def printTree(node): 
+    if(node == None): return 
+    print node.toString()
+    printTree(node.leftChild)
+    printTree(node.rightChild)
+
 
 
 ##################################### Main ###################################
@@ -126,11 +159,6 @@ print "ncol = ", ncol
 counts = getCounts(data)
 print("counts = ", counts)
 
-#calculation of H(Y|X)
-for i in range(len(counts) - 1): 
-	print("count = ", counts[i])
-	print("H(Y|X = )", calH(counts, i))
-
 #Test for finding the attribute with most information gain
 maxId = maximizedIG(counts)
 print("maxId = ", maxId)
@@ -138,7 +166,8 @@ print("maxId = ", maxId)
 #Test for split dataset
 dataOne, dataTwo = divMatrix(data, maxId)
 print(len(dataOne))
-
 print(len(dataTwo))
 
-
+node = buildTree(data, header, 0, None, None)
+#print(node.nodeData)
+printTree(node)
